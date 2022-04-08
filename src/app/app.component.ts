d4373ac9dcb06f0467361cef42c954543d27680f
Arun Sunny <sunnylistax25@gmail.com>
2022-04-08 19:27:14 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = '101195416_comp3133_assig2';
+}
